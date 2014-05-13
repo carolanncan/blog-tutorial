137e4434af9081e1bef62df552e02682de6333c0
carolanncan <carolanncan@gmail.com>
2014-05-13 00:31:03 -0700

    initial gh-pages commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'middleman', '~> 3.3.2'
+gem 'middleman-blog', '~> 3.5.2'
+gem "middleman-google-analytics"
+gem 'middleman-gh-pages'
+gem 'middleman-livereload', '~> 3.1.0'
+
+gem 'bitters'
+gem 'bourbon'
+gem 'font-awesome-sass'
+gem 'haml'
+gem 'jquery-middleman'
+gem 'neat'
+gem 'nokogiri'
+
+# For feed.xml.builder
+gem 'builder', '~> 3.0'
---
+++ config.rb
@@ -0,0 +1,52 @@
+# Blog settings
+
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  blog.sources = "posts/{year}-{month}-{day}-{title}.html"
+  blog.taglink = "tags/{tag}.html"
+  # blog.location = "location/{location}.html"
+  blog.layout = "main"
+  blog.summary_separator = /(READMORE)/
+    blog.summary_length = 150
+  blog.custom_collections = {
+    location: {
+    link: 'locations/{location}.html',
+    template: 'location.html'
+    }
+  }
+
+  activate :google_analytics do |ga|
+      ga.tracking_id = 'UA-XXXXXXX-X' # Replace with your property ID.
+  end
+
+  blog.tag_template = "tag.html"
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+
+# Helpers
+
+activate :automatic_image_sizes
+activate :livereload
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+set :relative_links, true
+
+# Build-specific configuration
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+  activate :relative_assets
+end
+
